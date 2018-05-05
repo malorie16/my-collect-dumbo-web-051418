e3fbeace90ecd12f5936df49b381045c08a02c36
malorie16 <malorie18@gmail.com>
2018-05-05 14:21:16 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,5 @@
 def my_collect(array)
   i = 0
-while i < array.length
   if array == languages
     languages.map  {|name| name.upcase}
 
@@ -8,6 +7,8 @@ while i < array.length
     students.map  {|name| name.split(" ").first}
 
   end
+while i < array.length
+
   yield(array[i])
 
   i = i + 1
